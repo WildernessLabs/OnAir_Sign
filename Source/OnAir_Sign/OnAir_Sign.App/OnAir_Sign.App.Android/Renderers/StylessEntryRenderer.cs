68047658fbef183b74972ae0dfe6cfc016572e73
Jorge Ramirez <ramirez.jorgea@outlook.com>
2021-03-05 00:47:23 -0800

    UI Updates

---
+++ Source/OnAir_Sign/OnAir_Sign.App/OnAir_Sign.App.Android/Renderers/StylessEntryRenderer.cs
@@ -0,0 +1,30 @@
+﻿using Android.Content;
+using OnAir_Sign.App.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(StylessEntryRenderer))]
+namespace OnAir_Sign.App.Droid.Renderers
+{
+    public class StylessEntryRenderer : EntryRenderer
+    {
+        public StylessEntryRenderer(Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                Control.Background = null;
+
+                var layoutParams = new MarginLayoutParams(Control.LayoutParameters);
+                layoutParams.SetMargins(0, 0, 0, 0);
+                LayoutParameters = layoutParams;
+                Control.LayoutParameters = layoutParams;
+                Control.SetPadding(0, 0, 0, 0);
+                Control.SetBackgroundColor(Color.Transparent.ToAndroid());
+                SetPadding(0, 0, 0, 0);
+            }
+        }
+    }
+}
---
+++ Source/OnAir_Sign/OnAir_Sign.App/OnAir_Sign.App.UWP/Renderers/StylessButtonRenderer.cs
@@ -0,0 +1,21 @@
+﻿using OnAir_Sign.App.UWP.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(Button), typeof(StylessButtonRenderer))]
+namespace OnAir_Sign.App.UWP.Renderers
+{
+    public class StylessButtonRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Resources["ButtonBackgroundPointerOver"] = Control.BackgroundColor;
+                Resources["ButtonBackgroundPressed"] = Control.BackgroundColor;
+            }
+        }
+    }
+}
